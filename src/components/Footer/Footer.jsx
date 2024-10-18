dbbeb9a01cc37a1f729d532b7cbce8dacd14236e
Yuvam Gupta <notsusdog@gmail.com>
2024-10-18 13:21:15 +0530

    Footer Twitter Icon modification

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -108,18 +108,8 @@ export default function Footer() {
                             <span className="sr-only">Discord community</span>
                         </Link>
                         <Link to="#">
-                            <svg
-                                className="w-4 h-4"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                viewBox="0 0 20 17"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
-                                    clip-rule="evenodd"
-                                />
+                            <svg className="w-4 h-4" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                 <path d="M178.57 127.15 290.27 0h-26.46l-97.03 110.38L89.34 0H0l117.13 166.93L0 300.25h26.46l102.4-116.59 81.8 116.59h89.34M36.01 19.54H76.66l187.13 262.13h-40.66"/>
                             </svg>
                             <span className="sr-only">Twitter page</span>
                         </Link>
